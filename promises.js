9688afa077b0a90b27725ac5d5485b76a804008d
unknown <yummyfunnybunny13@gmail.com>
2020-12-04 18:28:42 -0500

    added promises notes

---
+++ promises.js
@@ -0,0 +1,13 @@
+// ==============================
+// PROMISES
+// ==============================
+
+//  ASYN / AWAIT
+
+// Syntax with arrow functions
+const myFunc = async (el) => {
+  try{
+    const result = await request
+  } catch (err)
+}
+
